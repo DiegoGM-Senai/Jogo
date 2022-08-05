8ca698ede462edc47b9a1b492a079bb982cfea92
Diego <Diego_g_marques@estudante.sesisenai.org.br>
2022-08-05 17:52:19 -0300

    sla

--- src/components/Velocidade/index.js
+++ src/components/Velocidade/index.js
@@ -1,19 +1,57 @@
 import React, { useEffect, useRef } from "react";
+import ReactAudioPlayer from "react-audio-player";
+
 import "./Style.css";
 
 const Velocidade = (props) => {
+  const si = useRef(null);
   const Di = useRef(null);
   const Pe = useRef(null);
+  const x = useRef(null);
+  const y = useRef(null);
+  const res = useRef(null);
+
   useEffect(() => {
     const sauro = Di.current;
     const pedra = Pe.current;
-    console.log(sauro);
-    const jump = () => {
+    const sinal = si.current;
+    const num1 = x.current;
+    const num2 = y.current;
+    const resp = res.current;
+
+    let n1 = Math.floor(Math.random() * 10);
+    let n2 = Math.floor(Math.random() * 10);
+    let S = Math.floor(Math.random() * 4);
+
+    num1.value = n1;
+    num2.value = n2;
+
+    switch (S) {
+      case 0:
+        sinal.value = "+";
+
+        break;
+      case 1:
+        sinal.value = "-";
+
+        break;
+      case 2:
+        sinal.value = "/";
+
+        break;
+      case 3:
+        sinal.value = "*";
+
+        break;
+      default:
+    }
+
+    function calcular() {
       sauro.classList.add("jump");
       setTimeout(() => {
         sauro.classList.remove("jump");
       }, 1000);
-    };
+    }
 
     setInterval(() => {
       const posicaoPedra = pedra.offsetLeft;
@@ -28,7 +66,7 @@ const Velocidade = (props) => {
       clearInterval();
     }, 10);
 
-    document.addEventListener("keydown", jump);
+    // document.addEventListener("keydown", jump);
   });
 
   return (
@@ -37,11 +75,19 @@ const Velocidade = (props) => {
         <h1>JOGO</h1>
       </header>
       <div className="container">
-        <input type="number" id="x" readOnly />
-        <input type="text" id="sinal" readOnly />
-        <input type="number" id="y" readOnly />
-        <input type="number" id="resposta" placeholder="qual o resultado?" />
-        <button onClick={''}r>calculo</button>
+        <input type="number" ref={x} readOnly />
+        <input type="text" ref={si} readOnly />
+        <input type="number" ref={y} readOnly />
+        <input type="number" ref={res} placeholder="qual o resultado?" />
+
+        <button onClick={""}>calculo</button>
+
+        <ReactAudioPlayer
+          src={require("./som_fundo.mp3")}
+          autoPlay
+          loop
+          hidden
+        />
 
         <img
           ref={Di}
